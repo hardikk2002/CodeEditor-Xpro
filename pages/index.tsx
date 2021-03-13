d2f58dcf14d8e5bc058adb8db33da960f5381498
HARDIK KAUSHIK <58857069+hardikk2002@users.noreply.github.com>
2021-03-13 14:31:43 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,15 +11,6 @@ export default function Home() {
         <meta name="description" content="A Next-Generation Code Editor" />
         <meta name="keywords" content="Nextjs Typescript CodeEditor Socketio SCSS" />
         <meta name="author" content="Hardik Kaushik" />
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BW6QTRRX73"></script>
-        <script>
-         window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-BW6QTRRX73');
-        </script>
       </Head>
 
       <main className={styles.main}>
