3da80ba7b7706f0de7290e46c86445601d0de3e4
HARDIK KAUSHIK <58857069+hardikk2002@users.noreply.github.com>
2021-08-26 09:57:52 +0530

    ⭐️ Navbar links

--- pages/components/Navbar/Navbar.tsx
+++ pages/components/Navbar/Navbar.tsx
@@ -13,8 +13,8 @@ function Navbar() {
                     <nav className={styles.Nav}>
                         <ul>
                             <li><a href="https://hardikk2002.hashnode.dev/how-i-made-xpro-a-next-generation-code-editor">About</a></li>
-                            <li><a href="https://github.com/hardikk2002/CodeEditor-Xpro">Github</a></li>
-                            <li><a href="https://hardik28.netlify.app/">Developer</a></li>
+                            <li><a href="https://github.com/hardikk2002/CodeEditor-Xpro">⭐️ Star on Github</a></li>
+                            {\*<li><a href="">Developer</a></li>*/}
                         </ul>
                     </nav>
                 </div>
